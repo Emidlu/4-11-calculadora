a154fbfc3ef8808fac943ff9780e0c703a8a6668
Nahuel <naorlando@frba.utn.edu.ar>
2022-11-04 17:19:49 -0300

    multiplicacion y potencia

--- calculadora.py
+++ calculadora.py
@@ -15,6 +15,10 @@ numero1 = float(input("Ingrese un numero: "))
 numero2 = float(input("Ingrese otro numero: "))
 
 
+if operacion == 3:
+    print("el producto entre {} y {} es {}".format(numero1,numero2,(numero1*numero2)))
 
+if operacion == 5:
+    print("la potencia de {} elevado a {} es {}".format(numero1,numero2,(numero1**numero2)))
 
 
