ef679d168e7ece40d30256f54adac766149125ad
falvarenga <falvarenga@frba.utn.edu.ar>
2022-11-04 17:29:59 -0300

    division por 0

--- calculadora.py
+++ calculadora.py
@@ -8,11 +8,14 @@ print("5 - Para potenciar")
 print("6 - Para raiz")
 
 
-
 numero1 = float(input("Ingrese un numero: "))
 numero2 = float(input("Ingrese otro numero: "))
 operacion = int(input("Ingrese la operacion: "))
 
-
-
-
+def dividir(num,div):
+  return num/div
+if operacion == 4:
+    try:
+        print (dividir(numero1,numero2))
+    except ZeroDivisionError:
+        print("Trataste de dividir entre cero ")
