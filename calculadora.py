66629ee2acc56567062fe91c39d8f5ec45969002
Nahuel <naorlando@frba.utn.edu.ar>
2022-11-04 17:29:55 -0300

    segundo merge 2.0

--- calculadora.py
+++ calculadora.py
@@ -7,29 +7,22 @@ print("5 - Para potenciar")
 print("6 - Para raiz")
 
 
-<<<<<<< HEAD
 operacion = int(input("Ingrese la operacion: "))
 
 
 numero1 = float(input("Ingrese un numero: "))
 numero2 = float(input("Ingrese otro numero: "))
 
+if operacion == 1:
+    print(numero1 + numero2)
+if operacion == 2:
+    print(numero1 - numero2)
 
 if operacion == 3:
     print("el producto entre {} y {} es {}".format(numero1,numero2,(numero1*numero2)))
 
 if operacion == 5:
     print("la potencia de {} elevado a {} es {}".format(numero1,numero2,(numero1**numero2)))
-=======
-numero1 = float(input("Ingrese un numero: "))
-numero2 = float(input("Ingrese otro numero: "))
-operacion = int(input("Ingrese la operacion: "))
 
-
-if operacion == 1:
-    print(numero1 + numero2)
-if operacion == 2:
-    print(numero1 - numero2)
->>>>>>> 6ea88acab5427366920d12e293b78ce02d9b5fe7
 
 
