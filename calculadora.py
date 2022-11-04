c4e1f1116f5f849254a77e2d61af3dcee8d670e6
Emidlu <52872122+Emidlu@users.noreply.github.com>
2022-11-04 17:07:51 -0300

    Primer commit

---
+++ calculadora.py
@@ -0,0 +1,18 @@
+
+
+print("1 - Para sumar")
+print("2 - Para restar")
+print("3 - Para multiplicar")
+print("4 - Para dividir")
+print("5 - Para potenciar")
+print("6 - Para raiz")
+
+
+
+numero1 = float(input("Ingrese un numero: "))
+numero2 = float(input("Ingrese otro numero: "))
+operacion = int(input("Ingrese la operacion: "))
+
+
+
+
