6ea88acab5427366920d12e293b78ce02d9b5fe7
Emidlu <52872122+Emidlu@users.noreply.github.com>
2022-11-04 17:17:47 -0300

    Suma y resta

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,4 @@
 
-
 print("1 - Para sumar")
 print("2 - Para restar")
 print("3 - Para multiplicar")
@@ -8,11 +7,14 @@ print("5 - Para potenciar")
 print("6 - Para raiz")
 
 
-
 numero1 = float(input("Ingrese un numero: "))
 numero2 = float(input("Ingrese otro numero: "))
 operacion = int(input("Ingrese la operacion: "))
 
 
+if operacion == 1:
+    print(numero1 + numero2)
+if operacion == 2:
+    print(numero1 - numero2)
 
 
